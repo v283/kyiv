116ba0b81f9e5f4f4a8457035edf7d58328b9c51
Vannos-Slid <ivan.kush05@gmail.com>
2025-03-15 09:42:52 +0200

    merge with test

---
+++ kyiv/ViewModels/MultiselectionNotes.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using kyiv.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using kyiv.Services;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kyiv.ViewModels
+{
+    public partial class MultiselectionNotes : ObservableObject
+    {
+        public ObservableCollection<NoteModel> Notes { get; set; } = new();
+
+        private LocalDataService _databaseService;
+
+        public MultiselectionNotes()
+        {
+
+            _databaseService = new LocalDataService();
+            Notes = new ObservableCollection<NoteModel>(_databaseService.GetNotes());
+
+        }
+
+        [ObservableProperty]
+        private string _selectedNoteNumber;
+    }
+}
